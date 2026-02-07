86131b86439741d8541b0a9bc87124fd0a940684
Nicolas Jao <nicojao@icloud.com>
2026-02-07 00:58:54 -0500

    added LoRa stuff

---
+++ LoRa/Milestone 1 (LoRa Module Testing)/lora_a/lora_a.ino
@@ -0,0 +1,43 @@
+// Nicolas Jao - 400450150
+// Ayesha Mehmood - 400403482
+// Yassin Merdan - 400376619
+// Hans Qin - 400461329
+
+#include "lora_a.h"
+
+void setup() {
+
+  Serial.begin(9600);
+  Serial_1.begin(9600, SERIAL_8N1, 16, 17);
+
+  Transceiver.init();
+  Transceiver.SetAddressH(1);
+  Transceiver.SetAddressL(1);
+  Transceiver.SetAirDataRate(ADR_9600);
+  Transceiver.SetChannel(TRANSCEIVER_CHANNEL);
+  Transceiver.SetMode(EBYTE_MODE_NORMAL);
+  Transceiver.SetTransmitPower(OPT_TP20);
+  Transceiver.SaveParameters(PERMANENT);
+  Transceiver.PrintParameters();
+  
+}
+
+
+
+
+void loop() {
+  
+  if (Serial2.available() > 1) {
+    Serial.println("Receving Data...");
+    String incomingData = Serial2.readString();
+    Serial.println("PING: " + incomingData);
+
+  }
+
+  if(millis() - startTime > TIME_PER_SEND_MS){
+    Serial2.println(group_name + " - " + device_name);
+    Serial.println("PONG SENT");
+    startTime = millis();
+  }
+
+}
